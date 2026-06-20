dbca663a17dfc8ce771ee358f26e8f6985d83ec5
harshaggarwal0211-cmd <harshaggarwal0211@gmail.com>
2026-06-20 23:07:19 +0530

    Update labyrinth.cpp

--- graph/practice/labyrinth.cpp
+++ graph/practice/labyrinth.cpp
@@ -3,5 +3,26 @@
 using namespace std;
 #define F first
 #define S second
-using state pair<int,int>
-vector<int> 
+using state = pair<int,int>;
+vector<string> a;
+
+void solve(){
+    int n ,m ; cin>>n>>m;
+    a.resize(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    state st , en ;
+    for(int i=0;i<n;i++){
+        for(int j=0;i<m;j++){
+            if(a[i][j]=='A') st ={i,j};
+            else if (a[i][j]=='B') en ={i,j};
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    solve();
+}
